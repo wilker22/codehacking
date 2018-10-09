be9571c6ff8482a8923a8e889cd2bc6c4d91e7df
Wilker <wilker_a@hotmail.com>
2018-10-09 15:59:56 -0300

    commit com usuarios

---
+++ resources/views/admin/users/edit.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 08/10/2018
+ * Time: 17:07
+ */
--- resources/views/admin/users/index.blade.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: user
- * Date: 08/10/2018
- * Time: 16:13
- */
